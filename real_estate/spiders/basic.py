2e8aa6666407c34c0c34239d3cdb8f0c06bc35d9
Suhaib Janjua <suhaib.janjua@gmail.com>
2018-04-21 23:21:55 +0500

    using ItemLoader to make it visually more pleasant

--- real_estate/spiders/basic.py
+++ real_estate/spiders/basic.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.loader import ItemLoader
 from real_estate.items import RealEstateItem
 
 class BasicSpider(scrapy.Spider):
@@ -8,10 +9,12 @@ class BasicSpider(scrapy.Spider):
     start_urls = ['https://www.zameen.com/Homes/Lahore_Bahria_Town_Bahria_Town___Sector_A-1769-1.html']
 
     def parse(self, response):
-        item = RealEstateItem()
-        item['title'] = response.xpath('//*[@id="ls_title_10024233"][1]/text()').extract()
-        item['price'] = response.xpath('(//div[contains(@id, "ls_price")])[1]/font/text()').extract()
-        item['description'] = response.xpath('//*[@id="ls_property_link_10024233"]/div/div[2]/div[2]/div/div[1]/div[2]/div[2]/text()').extract()
-        item['address'] = response.xpath('//*[@id="ls_loc_9260436"]/text()').extract()
-        item['image_urls'] = response.xpath('//*[@id="ls_image_10024233"]/img/@src').extract()
-        return item
+        loader = ItemLoader(item=RealEstateItem(), response=response)
+
+        loader.add_xpath('title', '//*[@id="ls_title_10024233"][1]/text()')
+        loader.add_xpath('price', '(//div[contains(@id, "ls_price")])[1]/font/text()')
+        loader.add_xpath('description', '//*[@id="ls_property_link_10024233"]/div/div[2]/div[2]/div/div[1]/div[2]/div[2]/text()')
+        loader.add_xpath('address', '//*[@id="ls_loc_9260436"]/text()')
+        loader.add_xpath('image_urls', '//*[@id="ls_image_10024233"]/img/@src')
+        
+        return loader.load_item()
