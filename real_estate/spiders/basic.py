fa040d2d802af54d3c4f8d29354755b3c530b478
Suhaib Janjua <suhaib.janjua@gmail.com>
2018-04-21 20:42:35 +0500

    fetching data from zameen.com real estate website

--- real_estate/spiders/basic.py
+++ real_estate/spiders/basic.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+from real_estate.items import RealEstateItem
 
 class BasicSpider(scrapy.Spider):
     name = 'basic'
     allowed_domains = ['web']
-    start_urls = ['http://web/']
+    start_urls = ['https://www.zameen.com/Homes/Lahore_Bahria_Town_Bahria_Town___Sector_A-1769-1.html']
 
     def parse(self, response):
-        pass
+        item = RealEstateItem()
+        item['title'] = response.xpath('//*[@id="ls_title_10024233"][1]/text()').extract()
+        item['price'] = response.xpath('//*[@id="ls_price_9260436"]/font').extract()
+        item['description'] = response.xpath('//*[@id="ls_property_link_9260436"]/div/div[7]/text()').extract()
+        item['address'] = response.xpath('//*[@id="ls_loc_9260436"]/text()').extract()
+        item['image_urls'] = response.xpath('//*[@id="ls_image_10024233"]/img/@src').extract()
+        return item
