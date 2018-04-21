c553b092795195fec8b719415a6f658cf5dac625
Suhaib Janjua <suhaib.janjua@gmail.com>
2018-04-21 23:08:32 +0500

    property price and description scrapped acurately.

--- real_estate/spiders/basic.py
+++ real_estate/spiders/basic.py
@@ -10,8 +10,8 @@ class BasicSpider(scrapy.Spider):
     def parse(self, response):
         item = RealEstateItem()
         item['title'] = response.xpath('//*[@id="ls_title_10024233"][1]/text()').extract()
-        item['price'] = response.xpath('//*[@id="ls_price_9260436"]/font').extract()
-        item['description'] = response.xpath('//*[@id="ls_property_link_9260436"]/div/div[7]/text()').extract()
+        item['price'] = response.xpath('(//div[contains(@id, "ls_price")])[1]/font/text()').extract()
+        item['description'] = response.xpath('//*[@id="ls_property_link_10024233"]/div/div[2]/div[2]/div/div[1]/div[2]/div[2]/text()').extract()
         item['address'] = response.xpath('//*[@id="ls_loc_9260436"]/text()').extract()
         item['image_urls'] = response.xpath('//*[@id="ls_image_10024233"]/img/@src').extract()
         return item
